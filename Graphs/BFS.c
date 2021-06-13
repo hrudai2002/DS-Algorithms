e5f6b4051c09f2a1b13c11bcd15017da3508010e
Sree Sayi Hrudai <63182223+hrudai2002@users.noreply.github.com>
2021-06-13 13:10:54 +0530

    Update BFS.c

--- Graphs/BFS.c
+++ Graphs/BFS.c
@@ -35,22 +35,34 @@ void BFS() {
 }
 
 int main() {
-   /* input:
-      5 
-      0 1 1 1 0
-      1 0 1 0 1
-      1 1 0 0 1
-      1 0 0 0 1
-      0 1 1 1 0
-      
-      output:
-      1 2 3 4 5   */
-      scanf("%d",&n);  // No of vertices
-      for(int i = 1; i <= n; ++i) {  // Adjacency Matrix
-       for(int j = 1; j <= n; ++j) {
-              scanf("%d", &adj[i][j]);
-           }
+  int e;
+     scanf("%d", &e);
+    /* Input 1: 
+       when nodes are input
+        eg: 13 10 [no of vertices : 13, no of edges : 10]
+            1 2  // following are the edges between the nodes
+            1 3
+            1 4
+            2 7
+            2 8
+            2 9
+            3 10
+            3 11
+            4 12
+            4 13  */
+    while(e--) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    
+    // Input 2 : Matrix
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= n; ++j) {
+            scanf("%d",&adj[i][j]);
         }
+    }
       BFS(); // BFS traversal
       return 0;
 }
